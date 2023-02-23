16162e9dc98c62fcc9e8a0d94fc904a1ae855386
iggeehu <hu.igor@yahoo.com>
2023-02-23 12:34:31 -0500

    environment variable

--- secret.py
+++ secret.py
@@ -1,4 +1,8 @@
 import pandas
+from dotenv import load_dotenv
+import os
 
-secret = "82Husy82"
+load_dotenv(".env")
+dbPwd = os.getenv("MYSQLPWD")
+
 agentList = pandas.read_csv("agent-list.csv", header=0, usecols=["user_agent"])["user_agent"]
